83e2f1c9e8d5153d3794ba8c61f7f5efc79fdbbf
root <root@Tolu-PC.localdomain>
2022-09-14 23:43:26 +0100

    top

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
+#include "main.h"
+/*
+ * A simple program that prints (_putchar)
+ * using _putchar
+ */
 
-int main(void){
-	printf("_putchar");
-	printf("\n");
+int main(void)
+{
+	char word[] = "_putchar";
+	int i;
+
+	for (i = 0; i < strlen(word); i++)
+	{
+		_putchar("%c", word[i]);
+	}
+	_putchar("\n");
 	return (0);
 }
