5d00e6eba1cc02643758951d0da5b2ee0d05ac95
root <root@Tolu-PC.localdomain>
2022-09-15 00:24:03 +0100

    top

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always0.
+ */
+int main(void)
+{
+	char = a;
+	a = 'a';
+	while (a < 'z')
+	{
+		_putchar("%c", a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
