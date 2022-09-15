b9aa0918092d45ed243d3661464948512d43724f
root <root@Tolu-PC.localdomain>
2022-09-15 01:07:14 +0100

    jnfbbf

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10
  *
  * Return: Always 0.
  */
@@ -9,6 +9,7 @@
 void print_alphabet_x10(void)
 {
 	int i;
+
 	char a;
 	i = 0;
 
