6d93e86e7f50989bbe96aa55538f0b99a933d52d
root <root@Tolu-PC.localdomain>
2022-09-15 01:10:36 +0100

    jgnbslg

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10
+ * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: Always 0.
  */
 
 void print_alphabet_x10(void)
 {
-	int i;
-
 	char a;
+	int i;
 	i = 0;
 
 	while (i < 10)
 	{
 		a = 'a';
+		
 		while (a <= 'z')
 		{
 			_putchar(a);
