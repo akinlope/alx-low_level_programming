599c335b73dd947f94690840c5471d63bc23c3b7
root <root@Tolu-PC.localdomain>
2022-09-15 01:33:11 +0100

    jnbkjfb

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to check for capital character
+ *@c: is the character that will be used for the arguement of the function
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if (C >= "A" && C <= "Z"){
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
