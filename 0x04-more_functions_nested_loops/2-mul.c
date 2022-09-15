837824478abd4d4755ada88e267752f6bb6149f6
root <root@Tolu-PC.localdomain>
2022-09-15 17:11:29 +0100

    jhsbdj

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - a function that multiplies two integers
+ * @b: - arguement passed to the funtion
+ * @a: - test
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+
+	return (0);
+
+}
