7c60e43a49a0e3e9db55d24f6063b3d87732293d
root <root@Tolu-PC.localdomain>
2022-09-15 14:39:32 +0100

    hbskjbgejs

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check for uppercase
+ * @c: is the arguement passed into the function
+ * Return: 0
+*/
+
+int _isupper(int c)
+{
+
+	c = 'A';
+	if (c >= 'A' && c <= 'Z')
+	{
+		_putchar('\n');
+		return (1);
+	}
+	else
+	{
+		_putchar('\n');
+		return (0);
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A README.md file for the root of this folder
