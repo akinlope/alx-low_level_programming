fabb305d0a9d25e3756f3accd27ef92853a67387
root <root@Tolu-PC.localdomain>
2022-09-20 13:31:48 +0100

    kfhvsj

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - a function that returns the length of a string.
+ *
+ * *@s: parameter
+ *
+ * Return: always return length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
