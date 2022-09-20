e0c345bb912232af7f4b019f95064d686ac3a140
root <root@Tolu-PC.localdomain>
2022-09-20 14:47:30 +0100

    ubskjjs

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - a function that prints a string, in reverse.
+ *
+ * *@s: parameter
+ *
+ * Return: returns nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, len, temp;
+
+	len = strlen(s);
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
