dea061d7201416fd9fb5cf1f8267e523ed4e9c5f
root <root@Tolu-PC.localdomain>
2022-09-22 10:07:01 +0100

    hjvdj

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,21 @@
-#include "main.h"                                                                  /**
-										    * print_number - Prints an integer.                                                   
- * @n: The integer to be printed.                                                                                                     
- */                                                                                                                                   
-void print_number(int n)                                                                                                              
-{                                                                                                                                     
-        unsigned int num = n;                                                                                                         
-                                                                                                                                      
-        if (n < 0)                                                                                                                    
-        {                                                                                                                             
-                _putchar('-');                                                                                                        
-                num = -num;                                                                                                           
-        }                                                                                                                             
-                                                                                                                                      
-        if ((num / 10) > 0)                                                                                                           
-                print_number(num / 10);                                                                                               
-                                                                                                                                      
-        _putchar((num % 10) + '0');                                                                                                   
+#include "main.h"
+/**
+ * print_number - a funtion that prints an integer
+ * @n: arguement
+ * Return: always return nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar("-");
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
