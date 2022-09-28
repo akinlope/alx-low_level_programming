85e1336d3f6284cb3345f65c3b7b261b3234ae85
root <root@Tolu-PC.localdomain>
2022-09-28 14:17:43 +0100

    tip

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorials of n
+ * @n: arguement
+ * Return: returns a value
+ *
+ */
+
+int factorial(int n)
+{
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1 * factorial(n - 1));
+	}
+}
