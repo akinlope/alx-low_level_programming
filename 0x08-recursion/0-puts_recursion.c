b916a17fdf5457d12e264556e737efc6d30b58f4
root <root@Tolu-PC.localdomain>
2022-09-28 12:38:28 +0100

    top

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "_putchar"
+
+/**
+ * _puts_recursion - a function that prints a string
+ * *@s: arguement
+ * Return: return value
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar("%s\n", s);
+	}
+	else
+	{
+		putchar("\n");
+	}
+}
