1f2714ea2c263dc462133420d02f7e4459bc4848
root <root@Tolu-PC.localdomain>
2022-09-29 10:05:48 +0100

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - funtion to solve power
+ * @x: first arguement
+ * @y: second arguement
+ * Return: returns a value
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
