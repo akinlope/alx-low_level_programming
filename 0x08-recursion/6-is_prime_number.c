9fb2e91f06d56d27550599937a3ab6e8f9dff171
root <root@Tolu-PC.localdomain>
2022-09-30 14:53:58 +0100

    hfbv

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * myfunc - a sudo functio
+ * @n: first arguement
+ * @i: second arguement
+ * Return: return a value
+ *
+ */
+
+int myfunc(int n, int i)
+{
+	if (n % i == 0)
+	{
+		if (i == n)
+		{
+		return (1);
+		}
+	return (0);
+	}
+	return (0 + myfunc(n, i + 1));
+}
+
+/**
+ * _is_prime_number - check prime numbers
+ * @n: only arguement
+ * Return: returns a alue
+ *
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+	else if (n % 2 == 0)
+	{
+	return (0);
+	}
+	else
+	{
+	return (myfunc(n, 3));
+	}
+}
