32ef34f7253ba5d6b7dc8d6f7658f882af8fa537
root <root@Tolu-PC.localdomain>
2022-10-05 13:15:38 +0100

    updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function that counts argument passed in
+ * @argc: first argument
+ * @argv: second argument
+ * Return: always return 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+	}
+	
+	i = i - 2;
+	printf("%d\n", i);
+
+	return (0);
+}
+
