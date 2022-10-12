7311d52b8be2b0ade6ed88f850aa0bd8b60dc1fa
root <root@Tolu-PC.localdomain>
2022-10-12 09:56:20 +0100

    top

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
 
@@ -14,7 +13,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if(d == NULL)
+	if (d == NULL)
 	{
 		return;
 	}
