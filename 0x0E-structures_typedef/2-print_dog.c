f9025838b79bb95003bc501c8c34c744adbb0315
root <root@Tolu-PC.localdomain>
2022-10-12 14:42:07 +0100

    push

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,17 +9,18 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
-	{
-		printf("Name: (nil)");
-	}
-	else if (d->age == NULL)
-	{
-		printf("Age: (nill)");
-	}
-	else if (d->owner == NULL)
-	{
-		printf("Owner: (nil)");
-	}
+	if (d == 0)
+		return;
+	if (d->name == 0)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == 0)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+
 }
 
