8a56537a6c68a4c158ccce50d40bfe791967e33b
root <root@Tolu-PC.localdomain>
2022-10-12 14:50:03 +0100

    top

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,10 +2,11 @@
 #include "dog.h"
 
 /**
- * print_dog - a simple function
- * struct - a dog struct
- * @d: dog arg
- * Return: nothing
+ * print_dog - print a dog
+ *
+ * @d: dog to print
+ * 
+ * Return: void
  */
 
 void print_dog(struct dog *d)
